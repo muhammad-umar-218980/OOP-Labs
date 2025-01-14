c584c812893b3f2c757a5eb0c0f586adb07f5fb8
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-01-14 08:16:20 +0500

    Lab 1 added
    OOP Lab 1 all Questions done

---
+++ OOP Lab 1/OOP Lab 1 Q5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+float pi = 3.14159;
+
+double surfaceArea(double radius);
+double volume(double radius);
+
+int main() {
+    double radius;
+
+    cout << "Enter the radius of the sphere: ";
+    cin >> radius;
+
+    cout << "Surface Area: " << surfaceArea(radius) << endl;
+    cout << "Volume: " << volume(radius) << endl;
+
+    return 0;
+}
+
+double surfaceArea(double radius) {
+    float area =  4 * pi * radius * radius;
+    return area;
+}
+
+double volume(double radius) {
+    float area =  (4.0 / 3.0) * pi * radius * radius * radius;
+    return area ;
+}
---
+++ OOP Lab 1/OOP Lab 1 Q6.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char accountType;
+    int accountNumber;
+    double withdrawalAmount, remainingBalance;
+
+    remainingBalance = 200000;
+    
+    cout << "The Total Amount in your Account is " << remainingBalance << "\n" << endl;
+
+    cout << "Enter account type (S for savings, C for current): ";
+    cin >> accountType;
+
+    switch(accountType) {
+        case 'S':
+        case 's':
+            cout << "\n\nEnter savings account number: ";
+            cin >> accountNumber;
+
+            cout << "\nEnter withdrawal amount: ";
+            cin >> withdrawalAmount;
+
+            if (withdrawalAmount > 100000) {
+                cout << "\nCannot withdraw more than 100,000." << endl;
+            } else if (withdrawalAmount <= remainingBalance) {
+                double tax = 0;
+
+                remainingBalance = remainingBalance - withdrawalAmount;
+
+                if (withdrawalAmount > 50000) {
+                    tax = 0.05 * withdrawalAmount;
+                } else {
+                    tax = 0.02 * withdrawalAmount;
+                }
+
+                remainingBalance = remainingBalance - tax;
+
+                cout << "\nWithdrawal amount: " << withdrawalAmount << endl;
+                cout << "Tax deducted: " << tax << endl;
+                cout << "Remaining balance: " << remainingBalance << endl;
+            } else {
+                cout << "\n\nInsufficient balance." << endl;
+            }
+            break;
+
+        case 'C':
+        case 'c':
+            cout << "\n\nEnter current account number: ";
+            cin >> accountNumber;
+
+            cout << "\nEnter withdrawal amount: ";
+            cin >> withdrawalAmount;
+
+            if (withdrawalAmount > 100000) {
+                cout << "\nCannot withdraw more than 100,000." << endl;
+            } else if (withdrawalAmount <= remainingBalance) {
+                remainingBalance = remainingBalance - withdrawalAmount;
+                remainingBalance = remainingBalance - 100;
+
+                if (withdrawalAmount > 50000) {
+                    remainingBalance = remainingBalance - (0.05 * withdrawalAmount);
+                }
+
+                cout << "\nWithdrawal amount: " << withdrawalAmount << endl;
+                cout << "Fee deducted: 100" << endl;
+                cout << "Tax deducted: " << (withdrawalAmount > 50000 ? 0.05 * withdrawalAmount : 0) << endl;
+                cout << "Remaining balance: " << remainingBalance << endl;
+            } else {
+                cout << "\n\nInsufficient balance." << endl;
+            }
+            break;
+
+        default:
+            cout << "Invalid account type." << endl;
+            break;
+    }
+
+    return 0;
+}
